4dc3091ba38bd483cafbfb641deb0a7dbf8639c4
feuphraim <feuphraim@gmail.com>
2025-03-02 10:26:16 +0100

    Terraform State Commands

---
+++ Remote State/main-super-pets.tf
@@ -0,0 +1,8 @@
+resource "random_pet" "ultra_pet" {
+    length = var.length1
+    prefix = var.prefix1
+}
+resource "random_pet" "super_pet_2" {
+    length = var.length2
+    prefix = var.prefix2
+}
